384011fd99da9d43e3fd01f4845e0c585202b7f3
takedatsutomu <mdhd.to@gmail.com>
2020-05-01 00:02:40 +0900

    0430

---
+++ 0430.rb
@@ -0,0 +1,37 @@
+
+メールアドレス
+
+a = gets.chomp!
+b = gets
+
+puts a.to_s + "@" + b.to_s
+
+
+a = gets.chomp!
+b = gets.split(' ')
+
+p b[0].to_i * b[1].to_i % a.to_
+
+
+試験問題
+
+a = gets.split(' ')
+
+p a[0].to_i
+
+p a[0].to_i + a[1].to_i
+
+
+解答
+
+a = gets.split(' ')
+
+p a[0].to_i
+
+p a[0].to_i + a[1].to_i
+
+input line = gets.chomp!.split("").map(&:to_i)
+
+
+
+
