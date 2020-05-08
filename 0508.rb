4172d79d8ea03e9fecf802a875ee18febf064765
takedatsutomu <mdhd.to@gmail.com>
2020-05-08 16:43:08 +0900

    0508_d問題_3問

--- 0508.rb
+++ 0508.rb
@@ -16,3 +16,14 @@ p Prime.each(20000).to_a.sum
 line = gets.to_i
 
 p 100 + line*10
+
+# サイコロ
+num = gets.to_i
+p (7 - num)
+
+# 気温差
+line = gets.split(' ').map(&:to_i)
+p line[0]-line[1]
+
+line = gets.split(' ').map(&:to_i) 
+# 一行に複数、スペース消して、数字に
