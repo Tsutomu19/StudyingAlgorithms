1e893b96bb7b4d0e0f0784df1171b1597c061b7c
takedatsutomu <mdhd.to@gmail.com>
2020-05-08 17:30:58 +0900

    0508_c016

--- 0508.rb
+++ 0508.rb
@@ -27,3 +27,78 @@ p line[0]-line[1]
 
 line = gets.split(' ').map(&:to_i) 
 # 一行に複数、スペース消して、数字に
+
+
+n = gets.split(' ').map(&:to_i)
+m = gets.split(' ').map(&:to_i)
+p n[0] * m[1] - n[1] * m[0]
+
+
+s = gets.split(' ')
+if s = "A"
+    "A" = 4
+end
+
+p s
+
+
+s = gets.split(' ')
+s = gets.split('')
+# 上記は意味が違う
+
+
+
+
+
+# 回答
+input_line = gets.split('')
+arr = []
+input_line.map do |one|
+    if one == "A"
+        arr.push("4")
+        elsif one == "E"
+        arr.push("3")
+        elsif one == "G"
+        arr.push("6")
+        elsif one == "I"
+        arr.push("1")
+        elsif one == "O"
+        arr.push("0")
+        elsif one == "S"
+        arr.push("5")
+        elsif one == "Z"
+        arr.push("2")
+    else
+        arr.push(one)
+    end
+end
+puts arr.join
+
+
+
+# 参考
+input_line = gets.chomp!.split("")
+arr = []
+input_line.map do |one|
+    # 一個ずつ見てるんだ！！！
+  if one == "A"
+      arr.push("4")
+      elsif one == "E"
+      arr.push("3")
+      elsif one == "G"
+      arr.push("6")
+      elsif one == "I"
+      arr.push("1")
+      elsif one == "O"
+      arr.push("0")
+      elsif one == "S"
+      arr.push("5")
+
+    elsif one == "Z"
+      arr.push("2")
+  else
+      arr.push(one)
+  end
+
+end
+puts arr.join
