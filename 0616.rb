12b573c929509c1f5430e87daa27a5c41b534855
takedatsutomu <mdhd.to@gmail.com>
2020-06-16 10:54:50 +0900

    [90点-1]D100

--- 0616.rb
+++ 0616.rb
@@ -33,3 +33,60 @@ end
 
 paizaレーティング
 1216
+paizaレーティング
+1216
+
+
+
+D100:区切り文字の統一
+あなたはソースコードを読んでいる時に区切り文字として「_」(アンダースコア)と「-」(ハイフン)が混在している文字列の存在に気が付きました。
+そこで「_」か「-」どちらか一方に統一するため以下のルールで変換することにしました。
+
+・「_」と「-」のうち数が多い方に統一
+・もし同数の場合は「_」に統一
+
+ruby_python-java-php
+
+
+text = gets.split("")
+under,hyphen = 0,0
+text.each do |t|
+    if t == "_"
+        under += 1
+    elsif t == "-"
+        hyphen += 1
+    end
+end
+text.join("")
+if under >= hyphen
+    puts text.gsub!("-","_")
+else
+    puts text.gsub!("_","-")
+end
+
+
+
+
+最終的な回答
+text = gets.split("")
+under,hyphen = 0,0
+text.each do |t|
+    if t == "_"
+        under += 1
+    elsif t == "-"
+        hyphen += 1
+    end
+end
+
+if under >= hyphen
+    puts text.join("").gsub!("-","_")
+else
+    puts text.join("").gsub!("_","-")
+end
+
+
+paizaレーティング
+1196
+
+
+
