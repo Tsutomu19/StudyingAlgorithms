68b1f01319513c5349ca34236baf5da39aa5a016
takedatsutomu <mdhd.to@gmail.com>
2020-06-18 11:48:27 +0900

    B054

--- 0617.rb
+++ 0617.rb
@@ -201,3 +201,286 @@ puts gets.to_i.between?(40,60) ? "OK":"NG"
 
 
 
+
+
+
+B054:不思議な足し算
+
+S_1 と S_2 を足し算した結果を PAIZA 表記で 1 行に出力してください。
+出力の最後に改行を入れ、余計な文字、空行を含んではいけません。
+
+・"A", "B", "C", "D", "E" のみからなる文字列である。
+・0 以上の整数を表す。
+・"A" が 0、"B" が 1、"C" が 2、"D" が 3、"E" が 4 を表す 5 進数である。
+・0 以外の数字を表す際に、先頭に "A" が表記されることは許されていない。
+
+初期
+input = gets.split(" ")
+aa = input[0].split("")
+bb = input[1].split("")
+aa.each do |a|
+    if a == "A"
+
+    end
+end
+
+# 3つのフェーズがある。これはわかっていた。それで10進数にするにはどうすればいいのか？？思考停止
+# 進数変えるのめっちゃ便利だった
+input = gets.split(" ").map{|i| i.split("")}
+
+input.each do |paiza_num|
+    paiza_num.map! do |pp|
+        if pp == "A"
+            pp = 0
+        elsif pp == "B"
+            pp = 1
+        elsif pp == "C"
+            pp = 2
+        elsif pp == "D"
+            pp = 3
+        elsif pp == "E"
+            pp = 4
+        end
+    end
+end
+# p data
+# [[1, 4, 4], [2, 3]]
+ten = input[0].join("").to_i(10) + input[1].join("").to_i(10)
+# 5進数で3141だったら
+# 3*125(5^3) + 1*25(5^2) + 4 * 5 
+# 01234
+
+ten.each do |tt|
+    tt.map! do |t|
+        if t == 0
+            t = "A"
+        elsif t == 1
+            t = "B"
+        elsif t == 2
+            t = "C"
+        elsif t == 3
+            t = "D"
+        elsif t == 4
+            t = "E"
+        end
+    end
+end
+p ten
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def dec(i, 2)
+    return nil if n < 2 or n > 10
+    x = 0
+    for j in 0..(i.length - 1)
+      return nil unless (a = i[-(j + 1)]).between?("0", "9")
+      return nil if (b = a.to_i) >= n
+      x += (n ** j) * b
+    end
+    x
+  rescue
+    nil
+  end
+  
+  p dec("203", 5)   #=>53
+
+
+
+  input = gets.split(" ").map{|i| i.split("")}
+
+input.each do |paiza_num|
+    paiza_num.map! do |pp|
+        if pp == "A"
+            pp = 0
+        elsif pp == "B"
+            pp = 1
+        elsif pp == "C"
+            pp = 2
+        elsif pp == "D"
+            pp = 3
+        elsif pp == "E"
+            pp = 4
+        end
+    end
+end
+
+p ten = input.map{|a| a.join.to_i(5)}.sum.to_s(5)
+
+
+# ten.each do |tt|
+#     tt.map! do |t|
+#         if t == 0
+#             t = "A"
+#         elsif t == 1
+#             t = "B"
+#         elsif t == 2
+#             t = "C"
+#         elsif t == 3
+#             t = "D"
+#         elsif t == 4
+#             t = "E"
+#         end
+#     end
+# end
+# p ten
+
+
+
+
+
+input = gets.split(" ").map{|i| i.split("")}
+
+input.each do |paiza_num|
+    paiza_num.map! do |pp|
+        if pp == "A"
+            pp = 0
+        elsif pp == "B"
+            pp = 1
+        elsif pp == "C"
+            pp = 2
+        elsif pp == "D"
+            pp = 3
+        elsif pp == "E"
+            pp = 4
+        end
+    end
+end
+
+p ten = input.map{|a| a.join.to_i(5)}.sum.to_s(5)
+
+
+def change(tt)
+    ten.each do |tt|
+        tt.map! do |t|
+            if t == 0
+                t = "A"
+            elsif t == 1
+                t = "B"
+            elsif t == 2
+                t = "C"
+            elsif t == 3
+                t = "D"
+            elsif t == 4
+                t = "E"
+            end
+        end
+    end
+end
+
+change(ten)
+
+
+
+
+
+input = gets.split(" ").map{|i| i.split("")}
+
+input.each do |paiza_num|
+    paiza_num.map! do |pp|
+        if pp == "A"
+            pp = 0
+        elsif pp == "B"
+            pp = 1
+        elsif pp == "C"
+            pp = 2
+        elsif pp == "D"
+            pp = 3
+        elsif pp == "E"
+            pp = 4
+        end
+    end
+end
+
+ten = input.map{|a| a.join.to_i(5)}.sum.to_s(5)
+
+def change(tt)
+        tt.split("").map! do |t|
+        if t == "0"
+            t = "A"
+        elsif t == "1"
+            t = "B"
+        elsif t == "2"
+            t = "C"
+        elsif t == "3"
+            t = "D"
+        elsif t == "4"
+            t = "E"
+        end
+    end
+end
+
+puts change(ten).join("")
+
+
+
+
+
+
+
+# yuki
+# ・"A", "B", "C", "D", "E" のみからなる文字列である。
+# ・0 以上の整数を表す。
+# ・"A" が 0、"B" が 1、"C" が 2、"D" が 3、"E" が 4 を表す 5 進数である。
+# ・0 以外の数字を表す際に、先頭に "A" が表記されることは許されていない。
+
+data = gets.split(" ").map{|a| a.split("")}
+
+data.each do |f|
+    f.map! do |g|
+        if g == "A"
+            g = 0
+        elsif g == "B"
+            g = 1
+        elsif g == "C"
+            g = 2
+        elsif g == "D"
+            g = 3
+        elsif g == "E"
+            g = 4
+        end
+    end
+end
+
+def change_paiza(num)
+        num.split("").map! do |g|
+        if g == "0"
+            g = "A"
+        elsif g == "1"
+            g = "B"
+        elsif g == "2"
+            g = "C"
+        elsif g == "3"
+            g = "D"
+        elsif g == "4"
+            g = "E"
+        end
+    end
+end
+
+puts change_paiza(data.map{|a| a.join.to_i(5) }.sum.to_s(5)).join
+
+
+
+
+
+
+
+
+
+
