b00b445bf024437b48e359d33ec2d740c38d4576
takedatsutomu <mdhd.to@gmail.com>
2020-06-21 15:02:26 +0900

    C024

--- 0620.rb
+++ 0620.rb
@@ -39,3 +39,35 @@ input = gets.split(" ").map(&:to_i)
 
 height,width = input[0],input[1]
 p height * width % family_num == 0 ? 0 : height * width % family_num
+
+
+
+# C024:ミニ・コンピュータ
+# 14:25-:44
+
+
+3
+SET 1 10
+SET 2 20
+ADD 40
+
+
+
+data = (1..gets.to_i).map{gets.chomp.split(" ")}
+data_A,data_B = 0,0
+# 1だったらdata_aに数字をセットしたい。
+data.each do |f,g,h|
+    if f == "SET"
+        if g == "1"
+            data_A = h.to_i
+        else
+            data_B = h.to_i
+        end
+    elsif f == "ADD"
+        data_B = data_A + g.to_i
+    else
+        data_B = data_A - g.to_i
+    end
+end
+puts "#{data_A} #{data_B}"
+# 問題文を読む力
