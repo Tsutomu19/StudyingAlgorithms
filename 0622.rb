67aa06dd665cf1939b388bce2ba060b664d135d0
takedatsutomu <mdhd.to@gmail.com>
2020-06-23 12:30:20 +0900

    D080

--- 0622.rb
+++ 0622.rb
@@ -1,3 +1,4 @@
 D038:試合の回数
 member = gets.to_i
 p member * (member - 1) / 2
+# リーグ戦の計算方法
---
+++ 0623.rb
@@ -0,0 +1,5 @@
+D080:忘年会の予算
+
+
+people = gets.split(" ").map(&:to_i)
+p (people[0] * 6000) + (people[1] * 4000)
