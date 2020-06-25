68c55b1fd2e4e20b2080aa708818e68f87f3114a
takedatsutomu <mdhd.to@gmail.com>
2020-06-25 11:57:45 +0900

    D105

--- 0625.rb
+++ 0625.rb
@@ -1,4 +1,18 @@
 D115:【エンジニア死滅コラボ問題】おかしの二等分
-
 okashi = gets.to_i
 p okashi.even? ? okashi / 2 : (okashi - 1) / 2
+
+
+D105:長さの一致
+
+# 1
+string_a,string_b = gets.split,gets.split
+puts string_a.count == string_b.count ? "Yes" : "No"
+
+# 2
+string_a,string_b = gets.split(""),gets.split("")
+puts string_a.count == string_b.count ? "Yes" : "No"
+
+# 3
+string_a,string_b = gets.chomp.split(""),gets.chomp.split("")
+puts string_a.count == string_b.count ? "Yes" : "No"
