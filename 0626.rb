0e1423a04f0e4ce7d502ef087ffde7060528da34
takedatsutomu <mdhd.to@gmail.com>
2020-06-26 21:29:31 +0900

    D103

--- 0626.rb
+++ 0626.rb
@@ -4,3 +4,12 @@ D123:自動でチャージ
 money = gets.to_i
 p money < 10000 ? money + 10000 : money
 
+
+
+D103:逆さ読み
+string = gets.chomp.split("")
+p string.reverse
+
+# 回答
+puts gets.chomp.split("").reverse.join
+# ok
