c66aac7be1af35bf496248eb665f17705a40fe15
takedatsutomu <mdhd.to@gmail.com>
2020-06-26 10:35:09 +0900

    C023

--- 0625.rb
+++ 0625.rb
@@ -28,11 +28,51 @@ puts string_a.count == string_b.count ? "Yes" : "No"
 
 tousenn = gets.split(" ").map(&:to_i)
 num = gets.to_i
-my_kuji = (1..num).map{gets.split(" ")}.map(&:to_i)
-# tousenの配列の中から同じ数字があればその数だけ出したい。
+my_kuji = (1..num).map{gets.split(" ").map(&:to_i)}
+# tousenの配列とmy_kujiの配列を比較して同じ数字があればその数を表示したい。
 my_kuji.each do |kuji|
     p (tousenn & kuji).uniq.count
 end
+# (array1 & array2).present?
+
+# をググって
+# 13ふんぐらいで溶けた。成長。
 
 
-array1 & array2).present?
+
+
+
+C039:古代の数式
+
+<///////+<<</+////
+→ 17 + 31 + 4 = 52
+
+
+ones_place_count = 0
+tens_place_count = 0
+paiza_moji = gets.chomp.split("")
+.each do |m|
+    if m == "<"
+        tens_place_count += 1
+    elsif m == "/"
+        ones_place_count += 1
+        next m == "+"
+    end
+end
+p tens_place_count * 10 + ones_place_count
+
+
+# yuki
+arr = gets.chomp.split("+")
+
+answer = []
+arr.each do |f|
+    ten = f.count("<")
+    one = f.count("/")
+    number = 10 * ten + one
+    answer << number
+end
+p answer.sum
+
+# splitするときにそうするのか.......
+# count("何か")便利だ
---
+++ 0626.rb
@@ -0,0 +1,5 @@
+
+D123:自動でチャージ
+
+money = gets.to_i
+p money < 10000 ? money + 10000 : money
