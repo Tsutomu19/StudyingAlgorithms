696719373bd097ec50dfbc50331db68bc954f3d3
takedatsutomu <mdhd.to@gmail.com>
2020-06-26 22:12:23 +0900

    C017:ハイアンドロー・カードゲーム

--- 0626.rb
+++ 0626.rb
@@ -13,3 +13,51 @@ p string.reverse
 # 回答
 puts gets.chomp.split("").reverse.join
 # ok
+
+
+C017:ハイアンドロー・カードゲーム
+7 3
+4
+7 1
+7 4
+5 3
+10 1
+
+# 問題理解するのに40分くらい........
+parent = gets.split(" ").map(&:to_i)
+num = gets.to_i
+(1..num).map{gets.split(" ").map(&:to_i)}
+.each do |c|
+    if parent[0] > c[0]
+        puts "High"
+    elsif parent[0] < c[0]
+        puts "Low"
+    else
+        if parent[1] < c[1]
+            puts "Low"
+        else parent[1] > c[1]
+            puts "High"
+        end
+    end
+end
+
+
+
+
+
+parent = gets.split(" ").map(&:to_i)
+num = gets.to_i
+(1..num).map{gets.split(" ").map(&:to_i)}
+.each do |c|
+    if parent[0] > c[0]
+        puts "High"
+    elsif parent[0] < c[0]
+        puts "Low"
+    else
+        if parent[1] < c[1]
+            puts "High"
+        else parent[1] > c[1]
+            puts "Low"
+        end
+    end
+end
