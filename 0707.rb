1b4870fc8b01ecc26c0c65248a13d69050e89d28
takedatsutomu <mdhd.to@gmail.com>
2020-07-07 20:22:38 +0900

    D131

--- 0707.rb
+++ 0707.rb
@@ -1,2 +1,19 @@
 D126
 p gets.split(" ").map(&:to_i).sum
+
+
+D131:脱出ゲーム
+
+array = []
+data = gets.chomp.split("").map(&:to_i)
+.each do |d|
+    if d == 1
+        array << "A"
+    elsif d == 2
+        array << "B"
+    else
+        array << "C"
+    end
+end
+
+puts array.join
